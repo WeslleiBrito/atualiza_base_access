c350459f098aabb24b08c325f0773e32cd9f410f
WeslleiBrito <wesllei.wbs@gmail.com>
2022-05-02 16:55:49 -0300

    pegando dados

--- limpa_planilha.py
+++ limpa_planilha.py
@@ -18,5 +18,12 @@ if __name__ == '__main__':
 
     sub_grupos = [x for x in planilha_base.dataframe().iloc[:, 0]]
     quantidade = [x for x in planilha_base.dataframe().iloc[:, 2]]
+    faturamento = [x for x in planilha_base.dataframe().iloc[:, 3]]
+    custo = [x for x in planilha_base.dataframe().iloc[:, 4]]
+    despesa_total = [x for x in planilha_base.dataframe().iloc[:, 5]]
+    despesa_unitaria = [x for x in planilha_base.dataframe().iloc[:, 6]]
     print(sub_grupos)
     print(quantidade)
+    print(faturamento)
+    print(despesa_total)
+    print(despesa_unitaria)
